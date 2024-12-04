a81ceec9af70f03386428f7f8d4292ba9f8a09d6
Theo-th <theo.thivillier@viacesi.fr>
2024-12-04 19:03:19 +0100

    Avant merge

--- programme/main.cpp
+++ programme/main.cpp
@@ -7,22 +7,18 @@
 using namespace std;
 
 int main(){
-    std::vector<std::vector<int>> grid(100, std::vector<int>(100));
+    std::vector<std::vector<int>> grid(50, std::vector<int>(50));
  
-    // for (int x = 0; x < 20; ++x) {
-    //     for (int y = 0; y < 20; ++y) {
-    //         if ((x+y)%2){
-    //             grid[x][y] = 1;
-    //         } else {
-    //             grid[x][y] = 0;
-    //         }
-    //     }
-    // }
-    //grid[2][0] = 1;
-    grid[0][0] = 1;
-    grid[1][0] = 1;
-    grid[0][1] = 1;
-    grid[2][0] = 1;
+    for (int x = 0; x < 20; ++x) {
+        for (int y = 0; y < 20; ++y) {
+            if ((x+y)%2){
+                grid[x][y] = 1;
+            } else {
+                grid[x][y] = 0;
+            }
+        }
+    }
+
     Console console = Console(&grid);
     Grille grille = Grille(grid);
     grille.ajouterObservateurs(&console);
