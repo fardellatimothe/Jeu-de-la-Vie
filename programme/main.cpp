ca1922b5b152a3a6f424985d83d6339113725b2b
fardellatimothe <timothe.fardella@viacesi.fr>
2024-12-04 16:24:40 +0100

    Update GestionFichier

--- programme/main.cpp
+++ programme/main.cpp
@@ -3,37 +3,18 @@
 #include <iostream>
 
 int main() {
-    GestionFichier gestionFichier("fichier_input.txt");
+    GestionFichier gestionFichier("fichier_input.txt","Test");
 
     // Lire l'état initial
     gestionFichier.lireEtatInitial();
 
-    // Afficher la matrice
-    std::cout << "Matrice lue depuis le fichier :\n";
-    gestionFichier.afficherMatrice();
-
-    // Sauvegarder l'état actuel dans un autre fichier
-    gestionFichier.sauvegarderEtat();
-
     std::vector<std::vector<int>> maa(5, std::vector<int>(10, 1));
-    std::vector<std::vector<int>> maaa(5, std::vector<int>(10, 2));
-
-    gestionFichier.setMatrice(&maa);
-
-    // Afficher la matrice
-    std::cout << "\n\nMatrice mise manuellement :\n";
-    gestionFichier.afficherMatrice();
+    // std::vector<std::vector<int>> maaa(5, std::vector<int>(10, 2));
 
     // Sauvegarder l'état actuel dans un autre fichier
-    gestionFichier.sauvegarderEtat();
+    gestionFichier.sauvegarderEtat(maa);
 
-    // test
-
-    gestionFichier.sauvegarderEtat(&maaa);
-
-    // Afficher la matrice
-    std::cout << "\n\nDeuxième matrice mise manuellement :\n";
-    gestionFichier.afficherMatrice();
+    // gestionFichier.sauvegarderEtat(maaa);
 
     return 0;
 }
