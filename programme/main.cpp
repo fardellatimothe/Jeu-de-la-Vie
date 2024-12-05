9ee8ea7073170005c60b9680115cd40ffb071b68
Theo-th <theo.thivillier@viacesi.fr>
2024-12-05 09:43:36 +0100

    Temps

--- programme/main.cpp
+++ programme/main.cpp
@@ -4,25 +4,28 @@
 #include "Console.h"
 #include "Observable.h"
 #include "GestionFichier.h"
-
+#include <chrono>
 using namespace std;
 
 int main(){
+    auto start = std::chrono::high_resolution_clock::now();
     GestionFichier gestionFichier("fichier_input.txt","Test");
     vector<std::vector<int>>* test = gestionFichier.lireEtatInitial();
 
     Console console = Console(test);
     Grille grille = Grille(*test);
     grille.ajouterObservateurs(&console);
-    console.afficherMatrice();
+    //console.afficherMatrice();
 
-    for (int i=0; i<20; i++){
+    for (int i=0; i<50; i++){
         grille.calculerProchaineIteration();
         gestionFichier.sauvegarderEtat(test);
     }
 
-    console.afficherMatrice();
-    
+    //console.afficherMatrice();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Temps d'exécution : " << duration.count() << " secondes" << std::endl;
 
     return 0;
 }
